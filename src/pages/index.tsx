4d7676b35d6a10ab86a8d1978a7c680793ddcda0
takasaki376 <takasaki.a201070@outlook.jp>
2023-01-22 11:19:02 +0900

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <h1 className="text-xl font-bold underline text-red-600 bg-gray-200">
+      Hello world!
+    </h1>
+  )
+}
+export default Home
