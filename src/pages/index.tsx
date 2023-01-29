1b22c6b1e01afd9897e29a2475d04e3faade914d
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2023-01-29 13:25:06 +0900

    🐛Remove dead code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
 import type { NextPage } from 'next'
 
-type obj = {
-  name:string,
-  a: number,
-  id:number,
-  }
 const Home: NextPage = () => {
   return (
     <h1 className="text-xl font-bold underline text-red-600 bg-gray-200">
