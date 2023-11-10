7f7b4855dfc35b9d78205cb62c472c52dc4c4dea
yoko-at-home <tw107sf.ham@icloud.com>
2023-11-10 20:40:17 +0900

    markdownの設定

---
+++ src/app/test2/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+
+import { RichEditor } from '@/component/RichEditor'
+
+
+const TestPage2:NextPage = ()=> {
+    return (
+         <RichEditor />
+    )
+}
+
+export default TestPage2
