7c359cf9e205e9b87c08ca56cad446c23a96d3b8
yoko-at-home <tw107sf.ham@icloud.com>
2023-12-03 21:47:34 +0900

    create test3 page

---
+++ src/pages/test3/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+
+import {Tiptap} from '../../component/TipTap'
+
+const TestPage:NextPage = ()=> {
+    return (
+         <Tiptap />
+    )
+}
+
+export default TestPage
