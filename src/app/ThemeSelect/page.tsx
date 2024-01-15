bec1d0b7c1167c81beda5973b42543b049f6d180
yoko-at-home <tw107sf.ham@icloud.com>
2024-01-15 20:35:14 +0900

    10このテーマをランダム表示

--- src/app/ThemeSelect/page.tsx
+++ src/app/ThemeSelect/page.tsx
@@ -1,5 +1,39 @@
+import { theme } from "@/mock/theme";
+
 const ThemeSelectPage = () => {
-  return <div className="text-gray">ThemeSelectPage</div>;
+  const selected = randomSelect(theme.slice(), 10);
+
+  // 配列themeからランダムにnum個の要素を取り出す
+  function randomSelect(theme: any, num: number) {
+    const newTheme = [];
+
+    while (newTheme.length < num && theme.length > 0) {
+      // 配列からランダムな要素を選ぶ
+      const rand = Math.floor(Math.random() * theme.length);
+      // 選んだ要素を別の配列に登録する
+      newTheme.push(theme[rand]);
+      // もとの配列からは削除する
+      theme.splice(rand, 1);
+    }
+
+    return newTheme;
+  }
+  return (
+    <div className="">
+      <ul className="mx-auto w-full flex-col items-center justify-center p-3">
+        {selected.map((item) => {
+          return (
+            <div
+              className="flex w-full items-center justify-center p-3"
+              key={item.theme}
+            >
+              {item.theme}
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default ThemeSelectPage;
