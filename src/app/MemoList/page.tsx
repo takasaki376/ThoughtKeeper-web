0c236086f070ffbaca4776e661a18af5cb6f4daa
yoko-at-home <tw107sf.ham@icloud.com>
2024-02-11 23:03:19 +0900

    型の指定及びの作成日名称修正

--- src/app/MemoList/page.tsx
+++ src/app/MemoList/page.tsx
@@ -24,7 +24,7 @@ const MemoListPage = () => {
               key={item.theme}
             >
               <li className="mb-2 flex justify-around">
-                <div className="w-28"> {item.date}</div>
+                <div className="w-28"> {item.createdAt}</div>
                 <div className="mx-2 line-clamp-1 w-2/3">{item.memo}</div>
                 <div className="w-1/3">({item.theme})</div>
               </li>
---
+++ src/types/memo.ts
@@ -0,0 +1,6 @@
+export type Memo = {
+  title: string;
+  createdAt: string;
+  memo: string;
+  theme: string;
+};
