0d035940a9589ec2ad85956eba7c3ea6ff9bdf45
yoko-at-home <tw107sf.ham@icloud.com>
2024-03-31 19:59:50 +0900

    remove type

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,10 +1,10 @@
-import type { NextAuthOptions } from "next-auth";
+// import type { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 import { auth } from "@/firebase/admin";
 
-export const authOptions: NextAuthOptions = {
+export const authOptions = {
   callbacks: {
     async jwt({ token, user }) {
       return { ...token, ...user };
@@ -19,7 +19,7 @@ export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       // eslint-disable-next-line no-unused-vars
-      authorize: async ({ idToken }: any, _req) => {
+      authorize: async ({ idToken }, _req) => {
         if (idToken) {
           try {
             const decoded = await auth.verifyIdToken(idToken);
