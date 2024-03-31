af2f8cc4f0bcd88e4b1442508747a31822057b53
takasaki376 <takasaki.a201070@outlook.jp>
2024-03-31 21:51:00 +0900

    firebaseAdapter

---
+++ src/app/api/[...nextauth]/firebaseAdapter/index.ts
@@ -0,0 +1,15 @@
+import { FirestoreAdapter, initFirestore } from "@auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
+import type { Adapter } from "next-auth/adapters";
+
+const firestore = initFirestore({
+  credential: cert({
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+  }),
+});
+
+const firebaseAdapter = FirestoreAdapter(firestore) as Adapter;
+
+export default firebaseAdapter;
---
+++ src/app/api/[...nextauth]/providers/index.ts
@@ -0,0 +1,17 @@
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const googleProvider = GoogleProvider({
+  clientId: process.env.GOOGLE_CLIENT_ID!,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+});
+
+export const gitHubProvider = GitHubProvider({
+  clientId: process.env.GITHUB_ID!,
+  clientSecret: process.env.GITHUB_SECRET!,
+});
+
+// export const emailProvider = EmailProvider({
+//   server: process.env.EMAIL_SERVER,
+//   from: process.env.EMAIL_FROM,
+// });
