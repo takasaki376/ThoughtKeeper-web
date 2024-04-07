94b627f3c1485f1a77161b156da978ffa7827d5e
takasaki376 <takasaki.a201070@outlook.jp>
2024-04-07 21:45:26 +0900

    privateKey modify

--- src/app/api/auth/[...nextauth]/firebaseAdapter/index.ts
+++ src/app/api/auth/[...nextauth]/firebaseAdapter/index.ts
@@ -5,7 +5,7 @@ import type { Adapter } from "next-auth/adapters";
 const firestore = initFirestore({
   credential: cert({
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     projectId: process.env.FIREBASE_PROJECT_ID,
   }),
 });
