c62203c4a95d05d3263204e43d6548d01ca70735
yoko-at-home <tw107sf.ham@icloud.com>
2024-04-21 17:03:34 +0900

    tailwind configの修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,20 @@ module.exports = {
   content: ["./src/**/**/*.{ts,tsx}"],
   plugins: [require("prettier-plugin-tailwindcss")],
   theme: {
-    colors: {
-      background: "hsl(var(--background))",
-      black: "#1A1523",
-      btn: {
-        background: "hsl(var(--btn-background))",
-        "background-hover": "hsl(var(--btn-background-hover))",
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        black: "#1A1523",
+        btn: {
+          background: "hsl(var(--btn-background))",
+          "background-hover": "hsl(var(--btn-background-hover))",
+        },
+        foreground: "hsl(var(--foreground))",
+        gray: "#6F6E77",
+        lightGray: "#DCDBDD",
+        tomato: "#CA3214",
+        white: "#FFFFFF",
       },
-      foreground: "hsl(var(--foreground))",
-      gray: "#6F6E77",
-      lightGray: "#DCDBDD",
-      tomato: "#CA3214",
-      white: "#FFFFFF",
     },
-    extend: {},
   },
 };
