63ea4f91ffba3098406c3d7f055707e4103b89a5
yoko-at-home <tw107sf.ham@icloud.com>
2024-06-21 23:59:09 +0900

    オプションをinputへ変更

--- src/app/(authenticated)/setting/page.tsx
+++ src/app/(authenticated)/setting/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { type ChangeEvent, useState } from "react";
-
+import { MdOutlineClose } from "react-icons/md";
 
 export default function SettingPage() {
   const [value, setValue] = useState("");
@@ -8,78 +8,78 @@ export default function SettingPage() {
     setValue(e.target.value);
   };
   return (
-    <div id="section2" className="mt-6 rounded bg-white p-8 lg:mt-0">
-      <form>
-        <div className="mb-6 md:flex">
-          <div className="md:w-1/3">
-            <label className="mb-3 block pr-4 font-bold  md:mb-0 md:text-left">
-              テーマの件数
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <div className="flex items-center">
-              <select
-                name=""
-                className="mr-2 block w-full bg-lightGray p-1 focus:bg-white"
-                id="my-select"
-              >
-                <option value="Default">10</option>
-                <option value="A">20</option>
-                <option value="B">30</option>
-                <option value="C">その他</option>
-              </select>
-              件
+    <div
+      id="section2"
+      className="mx-3 mt-6 flex justify-between bg-white p-8 shadow lg:mt-0"
+    >
+      <div className="flex-1">
+        <form>
+          <div className="mb-6 md:flex">
+            <div className="md:w-1/3">
+              <label className="mb-3 block pr-4 font-bold  md:mb-0 md:text-left">
+                テーマの件数
+              </label>
             </div>
+            <div className="md:w-2/3">
+              <div className="flex items-center">
+                <input
+                  name=""
+                  className="mr-2 block w-full bg-lightGray p-1 focus:bg-white"
+                  id="my-select"
+                  defaultValue={value}
+                />
+                件
+              </div>
 
-            <p className="py-2 text-sm text-gray">
-              １日に記入するテーマの件数を設定します
-            </p>
-          </div>
-        </div>
-
-        <div className="mb-6 md:flex">
-          <div className="md:w-1/3">
-            <label className=" mb-3 block pr-4 font-bold md:mb-0 md:text-left">
-              入力する時間
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <div className="flex items-center">
-              <select
-                name=""
-                className="mr-2 block w-full bg-lightGray p-1 focus:bg-white"
-                id="my-select"
-              >
-                <option value="Default">60</option>
-                <option value="A">90</option>
-                <option value="B">120</option>
-                <option value="C">その他</option>
-              </select>
-              秒
+              <p className="py-2 text-sm text-gray">
+                １日に記入するテーマの件数を設定します
+              </p>
             </div>
-            <p className="py-2 text-sm text-gray">
-              １つのテーマに対して、入力する時間を設定します
-            </p>
           </div>
-        </div>
 
-        <div className="mb-6 md:flex">
-          <div className="md:w-1/3">
-            <label className="mb-3 block pr-4 font-bold  md:mb-0 md:text-left">
-              テーマのデフォルト値
-            </label>
+          <div className="mb-6 md:flex">
+            <div className="md:w-1/3">
+              <label className=" mb-3 block pr-4 font-bold md:mb-0 md:text-left">
+                入力する時間
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <div className="flex items-center">
+                <input
+                  name=""
+                  className="mr-2 block w-full bg-lightGray p-1 focus:bg-white"
+                  id="my-select"
+                  defaultValue={value}
+                />
+                秒
+              </div>
+              <p className="py-2 text-sm text-gray">
+                １つのテーマに対して、入力する時間を設定します
+              </p>
+            </div>
           </div>
-          <div className="md:w-2/3">
-            <textarea
-              className="block w-full bg-lightGray p-1 focus:bg-white"
-              id="my-textarea"
-              defaultValue={value}
-              onChange={handleOnChange}
-            ></textarea>
-            <p className="py-2 text-sm text-gray ">説明</p>
+
+          <div className="mb-6 md:flex">
+            <div className="md:w-1/3">
+              <label className="mb-3 block pr-4 font-bold  md:mb-0 md:text-left">
+                テーマのデフォルト値
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <textarea
+                className="block w-full bg-lightGray p-1 focus:bg-white"
+                id="my-textarea"
+                defaultValue={value}
+                onChange={handleOnChange}
+              ></textarea>
+              <p className="py-2 text-sm text-gray ">説明</p>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
+      <div className="pl-16">
+        <MdOutlineClose className="w-20" />
+      </div>
     </div>
   );
 }
