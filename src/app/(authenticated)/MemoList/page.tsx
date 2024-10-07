77a76afd520e3a523b27e2c4970e1dcdec27a44c
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2024-10-07 20:09:56 +0900

    MemoListページのデータをローカルのDBから取得

--- src/app/(authenticated)/MemoList/page.tsx
+++ src/app/(authenticated)/MemoList/page.tsx
@@ -1,6 +1,8 @@
+"use client";
 import Link from "next/link";
 
 import { getThemes } from "@/pages/api/themes";
+import { themes } from "@/types/database";
 
 const PostList = ({
   memos,
@@ -11,20 +13,23 @@ const PostList = ({
     <div className="px-12">
       {memos.map((memo) => (
         <div className="line-clamp-1" key={memo.id}>
-          <Link href={`/MemoViewer/${memo.id}`}>{memo.theme}</Link>
+          <Link href={`/MemoViewer/${memo.id}`}>
+            {memo.title}：{memo.theme}
+          </Link>
         </div>
       ))}
     </div>
   );
 };
 
-export default async function Home() {
-  const themes = await getThemes(); // Supabaseからデータを取得
-
+export default async function MemoListPage() {
+  const theme = (await getThemes()) as themes; // Supabaseからデータを取得
+  const count = theme.length;
   return (
     <div>
       <h1>Supabaseからデータを表示</h1>
-      <PostList memos={themes} />
+      <div>テーマ数：{count}</div>
+      <PostList memos={theme} />
     </div>
   );
 }
