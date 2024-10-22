6adc1905a5fdd091652b37c0677ea3b672f85163
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2024-10-22 16:53:04 +0900

    保存されたメモを表示

--- src/app/(authenticated)/MemoList/page.tsx
+++ src/app/(authenticated)/MemoList/page.tsx
@@ -1,19 +1,33 @@
 "use client";
+import { useAtomValue } from "jotai";
 
-import { Loader } from "@/component/Loader";
-import { useGetThemes } from "@/hooks/useGetThemes";
+import { memoListAtom } from "@/store/setting";
 
-import MemoListPageClient from "./MemoListPageClient";
+// HTMLタグを除去する関数
+const stripHTML = (html: string) => {
+  const doc = new DOMParser().parseFromString(html, "text/html");
+  return doc.body.textContent || "";
+};
 
 export default function MemoListPage() {
-  const { error, loading, themes } = useGetThemes(); // フックを使用してデータを取得
+  const memoList = useAtomValue(memoListAtom); // 保存されたメモを取得
+  const reversedList = memoList.toReversed();
 
-  if (loading) {
-    return <Loader />;
-  }
-  if (error) {
-    return <div>{error}</div>;
-  }
-
-  return <MemoListPageClient memos={themes} count={themes.length} />;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="mb-5 text-xl font-bold">保存されたメモ</h1>
+      <ul className="w-2/3 list-disc">
+        {reversedList.map((memo, index) => (
+          <li key={index} className="mb-4">
+            <p className="text-sm text-gray">
+              日付: {memo.date}: {memo.time}
+            </p>
+            <p className="text-base font-semibold">テーマ: {memo.theme}</p>
+            {/* HTMLタグを除去したメモの内容を表示 */}
+            <p>{stripHTML(memo.content)} / </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
