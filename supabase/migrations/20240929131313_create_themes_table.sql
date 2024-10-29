f76beaf7301908599f4159aabb3c58e45323f21a
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2024-10-29 22:22:49 +0900

    ユーザー設定用テーブルの作成

--- supabase/migrations/20240929131313_create_themes_table.sql
+++ supabase/migrations/20240929131313_create_themes_table.sql
@@ -4,3 +4,10 @@ create table themes (
   theme text,
   primary key (id)
 );
+
+create table user_settings (
+  user_id uuid references auth.users (id) on delete cascade,
+  theme_count integer not null default 10,
+  time_limit text not null default '60',
+  primary key (user_id)
+);
