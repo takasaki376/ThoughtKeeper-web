69215a85353c3203e7dc53e2a4dc938e95c574c3
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2025-02-02 13:40:12 +0900

    tsエラーの修正

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,7 +1,12 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+export const createClient = () => {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Required environment variables are not set');
+  }
+
+  return createBrowserClient(supabaseUrl, supabaseKey);
+};
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -4,9 +4,16 @@ import { cookies } from "next/headers";
 export const createClient = () => {
   const cookieStore = cookies();
 
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Required environment variables are not set');
+  }
+
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    supabaseUrl,
+    supabaseKey,
     {
       cookies: {
         get(name: string) {
