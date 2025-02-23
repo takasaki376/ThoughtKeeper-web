acd14d0fd8dfd6f11c491e4591c65f06034a8eb7
takasaki376 <takasaki.a201070@outlook.jp>
2025-02-23 23:10:13 +0900

    環境変数名変更

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -12,8 +12,8 @@ export const updateSession = async (request: NextRequest) => {
       },
     });
 
-    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-    const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+    const supabaseUrl = process.env.SUPABASE_URL;
+    const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
     if (!supabaseUrl || !supabaseKey) {
       throw new Error('Required environment variables are not set');
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -4,8 +4,8 @@ import { cookies } from "next/headers";
 export const createClient = () => {
   const cookieStore = cookies();
 
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
   if (!supabaseUrl || !supabaseKey) {
     throw new Error('Required environment variables are not set');
